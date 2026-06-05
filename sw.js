c2491a65b74dc9e0f732e154db9d101df5a37628
codygaddie-dot <codygaddie@gmail.com>
2026-06-05 11:57:38 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = "recovery-v1";
+const ASSETS = ["/recovery/", "/recovery/index.html", "/recovery/manifest.json"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      if (res && res.status === 200) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match("/recovery/")))
+  );
+});
